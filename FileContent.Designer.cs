c97892f856617741d8a416ea1d0c7d6d7d19028e
Mohammad Khan <Mosiulhaque@gmail.com>
2022-05-30 08:46:35 +1000

    first commit

---
+++ FileContent.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace ReadingTextFiles
+{
+    partial class FileContent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblLetters = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWords = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblAvg = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGetFiles = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox1.Font = new System.Drawing.Font("AponaLohit", 9.216F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(142, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(710, 275);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblLetters
+            // 
+            this.lblLetters.AutoSize = true;
+            this.lblLetters.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLetters.Location = new System.Drawing.Point(540, 278);
+            this.lblLetters.Name = "lblLetters";
+            this.lblLetters.Size = new System.Drawing.Size(24, 25);
+            this.lblLetters.TabIndex = 2;
+            this.lblLetters.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(489, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Letters:";
+            // 
+            // lblWords
+            // 
+            this.lblWords.AutoSize = true;
+            this.lblWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWords.Location = new System.Drawing.Point(351, 278);
+            this.lblWords.Name = "lblWords";
+            this.lblWords.Size = new System.Drawing.Size(24, 25);
+            this.lblWords.TabIndex = 4;
+            this.lblWords.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(300, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Words:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(652, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Average:";
+            // 
+            // lblAvg
+            // 
+            this.lblAvg.AutoSize = true;
+            this.lblAvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAvg.Location = new System.Drawing.Point(712, 278);
+            this.lblAvg.Name = "lblAvg";
+            this.lblAvg.Size = new System.Drawing.Size(24, 25);
+            this.lblAvg.TabIndex = 7;
+            this.lblAvg.Text = "0";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel1.Controls.Add(this.btnGetFiles);
+            this.flowLayoutPanel1.Controls.Add(this.btnClear);
+            this.flowLayoutPanel1.Controls.Add(this.btnClose);
+            this.flowLayoutPanel1.Controls.Add(this.label6);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(141, 308);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.label3);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(133, 75);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "...........";
+            // 
+            // btnGetFiles
+            // 
+            this.btnGetFiles.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnGetFiles.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnGetFiles.FlatAppearance.BorderSize = 0;
+            this.btnGetFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGetFiles.Font = new System.Drawing.Font("Mina", 9.792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetFiles.ForeColor = System.Drawing.Color.Black;
+            this.btnGetFiles.Image = global::ReadingTextFiles.Properties.Resources.size;
+            this.btnGetFiles.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGetFiles.Location = new System.Drawing.Point(3, 84);
+            this.btnGetFiles.Name = "btnGetFiles";
+            this.btnGetFiles.Size = new System.Drawing.Size(133, 33);
+            this.btnGetFiles.TabIndex = 1;
+            this.btnGetFiles.Text = "Get File";
+            this.btnGetFiles.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGetFiles.UseVisualStyleBackColor = false;
+            this.btnGetFiles.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Mina", 9.792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.Black;
+            this.btnClear.Image = global::ReadingTextFiles.Properties.Resources.size;
+            this.btnClear.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClear.Location = new System.Drawing.Point(3, 123);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(133, 33);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Mina", 9.792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Black;
+            this.btnClose.Image = global::ReadingTextFiles.Properties.Resources.size;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(3, 162);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(133, 33);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.184F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Lavender;
+            this.label6.Location = new System.Drawing.Point(3, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 52);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Click GetFile and choose a text file to load. you can manually type in textbox to" +
+    "o. ";
+            // 
+            // FileContent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 308);
+            this.ControlBox = false;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.lblAvg);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblWords);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblLetters);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FileContent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FileContent_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGetFiles;
+        private System.Windows.Forms.Label lblLetters;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWords;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblAvg;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ FileContent.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ReadingTextFiles
+{
+    public partial class FileContent : Form
+    {
+        private string filePath;
+        private string fileContent;
+
+        public FileContent()
+        {
+            InitializeComponent();
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Both;
+            //Setup events that listens on keypress
+            textBox1.KeyDown += TextBox1_KeyDown;
+        }
+
+
+        // Handle the KeyDown event to print the type of character entered into the control.
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                LoadResult(textBox1.Text);                
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+                    using (StreamReader reader = new StreamReader(fileStream))                    
+                        fileContent = reader.ReadToEnd();                    
+                }
+            }
+            MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+            textBox1.Text = fileContent;
+            if(textBox1.Text != "")
+            LoadResult(fileContent);
+
+        }
+
+        
+        public int CountWords(string test)
+        {
+            var noOfWords = Regex.Replace(test, "[^a-zA-Z_]+", " ")
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Length;
+            return noOfWords;
+        }
+
+        public int countLetters(string test)
+        {
+            int totalLetterChar = 0;
+            char[] strArray = test.ToCharArray();
+            foreach (var item in strArray)
+            {
+                if (char.IsLetter(item))
+                    totalLetterChar++;
+            }
+            return totalLetterChar;
+        }
+
+        public void LoadResult(string fileContent)
+        {
+            lblWords.Text = (CountWords(fileContent)).ToString();
+            lblLetters.Text = (countLetters(fileContent)).ToString();
+            var x = Convert.ToInt32(lblWords.Text.Trim());
+            var y = Convert.ToInt32(lblLetters.Text.Trim());
+            var z = y / x;
+            lblAvg.Text = z.ToString();
+        }
+
+        private void FileContent_Load(object sender, EventArgs e)
+        {
+            
+            string a1 = Environment.UserName;
+                label1.Text = a1.ToString();
+            string a2 = (Environment.Is64BitOperatingSystem) ? "64 bit Operating System" : "32 bit Operating System";
+            label3.Text = a2.ToString();
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            lblWords.Text = "0";
+            lblLetters.Text = "0";
+            lblAvg.Text = "0";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        
+    }
+}
